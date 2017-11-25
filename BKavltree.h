c57f58f85d03b82df5a986613030313289ad8766
DangPhuTrungTin <trungtincse@gmail.com>
2017-11-25 12:56:56 +0700

    add lib TinyXML and adjunct code

---
+++ BKavltree.h
@@ -0,0 +1,306 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+using namespace std;
+enum status { LH = -1, EH, RH };
+struct node {
+	string key ="";
+	int numberSub=0;
+	node* left = NULL;
+	node* right = NULL;
+	status balance = EH;
+	node(string name, int value) { key = name; numberSub = value; }
+};
+node* rotateRight(node* & root) {
+	node* tempPtr = root->left;
+	root->left = tempPtr->right;
+	tempPtr->right = root;
+	return tempPtr;
+}
+node* rotateLeft(node* & root) {
+	node* tempPtr = root->right;
+	root->right = tempPtr->left;
+	tempPtr->left = root;
+	return tempPtr;
+}
+node* leftBalance(node* &root, bool &taller) {
+
+	node* leftTree = root->left;
+	//Case 1: L of L
+	if (leftTree->balance == LH)
+	{
+		root->balance = EH;
+		leftTree->balance = EH;
+		root = rotateRight(root); // single ratation right
+		taller = false;
+	}
+	else
+	{
+		node* rightTree = leftTree->right;
+		if (rightTree->balance == LH) {
+			root->balance = RH;
+			leftTree->balance = EH;
+		}
+		else if (rightTree->balance == EH)
+		{
+			leftTree->balance = EH; root->balance = EH;
+		}
+		else {
+			root->balance = EH;
+			leftTree->balance = LH;
+		}
+		rightTree->balance = EH;
+		root->left = rotateLeft(leftTree);
+		root = rotateRight(root);
+		taller = false;
+	}
+	return root;
+}
+node* rightBalance(node* &root, bool &taller) {
+	node* rightTree = root->right;
+	if (rightTree->balance == RH) {
+		root->balance = EH;
+		rightTree->balance = EH;
+		root = rotateLeft(root);
+
+
+		taller = false;
+	}
+	else {
+		node* leftTree = rightTree->left;
+		if (leftTree->balance == RH) {
+			root->balance = LH;
+			rightTree->balance = EH;
+		}
+		else if (leftTree->balance == EH) { rightTree->balance = EH; root->balance = EH; }
+		else {
+			root->balance = EH;
+			rightTree->balance = RH;
+		}
+		leftTree->balance = EH;
+		root->right = rotateRight(rightTree);
+		root = rotateLeft(root);
+		taller = false;
+	}
+	return root;
+}
+node* deleteRightBalance(node* & root, bool & shorter) {
+	if (!shorter) return root;
+	if (root->balance == LH) root->balance = RH;
+	else if (root->balance == EH) {
+		root->balance = RH;
+		shorter = false;
+	}
+	else {
+		node* rightTree = root->right;
+		if (rightTree->balance == LH) {
+			node* leftTree = rightTree->left;
+			if (leftTree->balance == LH) {
+				rightTree->balance = RH;
+				root->balance = EH;
+			}
+			else if (leftTree->balance == EH) {
+				root->balance = LH;
+				rightTree->balance = EH;
+			}
+			else {
+				root->balance = LH;
+				rightTree->balance = EH;
+			}
+			leftTree->balance = EH;
+			root->right = rotateRight(rightTree);
+			root = rotateLeft(root);
+		}
+		else {
+			if (rightTree->balance != EH) {
+				root->balance = EH;
+				rightTree->balance = EH;
+			}
+			else {
+				root->balance = RH;
+				rightTree->balance = LH;
+				shorter = false;
+			}
+			root = rotateLeft(root);
+		}
+	}
+	return root;
+}
+node* deleteLeftBalance(node* & root, bool &shorter) {
+	if (!shorter) return root;
+	if (root->balance == RH)root->balance = EH;
+	else if (root->balance == EH) {
+		root->balance = LH;
+		shorter = false;
+	}
+	else {
+		node* leftTree = root->left;
+		if (leftTree->balance == RH) {
+			node* rightTree = leftTree->right;
+			if (rightTree->balance == RH) {
+				leftTree->balance = LH;
+				root->balance = EH;
+			}
+			else if (rightTree->balance == EH) {
+				root->balance = RH;
+				leftTree->balance = EH;
+			}
+			else {
+				root->balance = RH;
+				leftTree->balance = EH;
+			}
+			rightTree->balance = EH;
+			root->left = rotateLeft(leftTree);
+			root = rotateRight(root);
+		}
+		else {
+			if (leftTree->balance != EH) {
+				root->balance = EH;
+				leftTree->balance = EH;
+			}
+			else
+			{
+				root->balance = LH;
+				leftTree->balance = RH;
+				shorter = false;
+			}
+			root = rotateRight(root);
+		}
+
+	}
+	return root;
+}
+class avlTree {
+public:
+	node* root = NULL;
+	node* AVLInsert(node*& root, node* newPtr, bool & taller) {
+		if (root == NULL) {
+			root = newPtr;
+			taller = true;
+			return root;
+		}
+		if (newPtr->key < root->key)
+		{
+			root->left = AVLInsert(root->left, newPtr, taller);
+			if (taller)
+			{
+				if (root->balance == LH) root = leftBalance(root, taller);
+				else if (root->balance == EH) root->balance = LH;
+				else {
+					root->balance = EH;
+					taller = false;
+				}
+			}
+		}
+		else {
+			root->right = AVLInsert(root->right, newPtr, taller);
+			if (taller) {
+				if (root->balance == LH) {
+					root->balance = EH;
+					taller = false;
+				}
+				else if (root->balance == EH) root->balance = RH;
+				else root = rightBalance(root, taller);
+			}
+		}
+		return root;
+	}
+	node* AVLDelete(node* & root, string deletekey, bool & shorter, bool & success) {
+		if (root == NULL) {
+			shorter = false;
+			success = false;
+			return NULL;
+		}
+		if (deletekey < root->key) {
+			root->left = AVLDelete(root->left, deletekey, shorter, success);
+			if (shorter)
+				root = deleteRightBalance(root, shorter);
+		}
+		else if (deletekey > root->key) {
+			root->right = AVLDelete(root->right, deletekey, shorter, success);
+			if (shorter) root = deleteLeftBalance(root, shorter);
+		}
+		else {
+			node* deleteNode = root;
+			if (root->right == NULL) {
+				node* newRoot = root->left;
+				success = true;
+				shorter = true;
+				delete deleteNode;
+				return newRoot;
+			}
+			else if (root->left == NULL) {
+				node* newRoot = root->right;
+				success = true;
+				shorter = true;
+				delete deleteNode;
+				return newRoot;
+			}
+			else {
+				node* exchPtr = root->left;
+				while (exchPtr->right != NULL)
+					exchPtr = exchPtr->right;
+				root->key = exchPtr->key;
+				root->left = AVLDelete(root->left, exchPtr->key, shorter, success);
+				if (shorter)
+					root = deleteRightBalance(root, shorter);
+			}
+		}
+		return root;
+	}
+	void print_preorder(node* root) {
+		if (root == NULL) return;
+		else
+		{
+			cout << "  " << root->key << "  ";
+			print_preorder(root->left);
+			print_preorder(root->right);
+		}
+	}
+	void print_inorder(node* root) {
+		if (root == NULL) return;
+		else
+		{
+			
+			print_inorder(root->left);
+			cout << "  " << root->key << "|" << root->numberSub << endl;
+			print_inorder(root->right);
+		}
+	}
+	void print_preorderbalance(node* root) {
+		if (root == NULL) return;
+		else
+		{
+			cout << "  " << root->balance << "  ";
+			print_preorderbalance(root->left);
+			print_preorderbalance(root->right);
+		}
+	}
+	bool taller = false;
+	bool shorter = false;
+	node* search(string value) {
+		if (root==NULL) return NULL;
+		node* current = root;
+		while (current->key != value) {
+			if (value > current->key) current = current->right;
+			else current = current->left;
+			if (current == NULL) return NULL;
+		}
+		return current;
+	}
+};
+bool operator >(string a, string b) {
+	if (a.compare(b) > 0) return true;
+	else return false;
+}
+bool operator <(string a, string b) {
+	if (a.compare(b) < 0) return true;
+	else return false;
+}
+bool operator !=(string a, string b) {
+	if (a.compare(b) != 0) return true;
+	else return false;
+}
+
+
--- test1.cpp
+++ test1.cpp
@@ -1,66 +1,132 @@
 #include <boost/filesystem.hpp>
 #include <iostream>
 #include <string>
-
+#include "tinyxml.h"
+#include "BKavltree.h"
 using namespace std;
-namespace fs = boost::filesystem;
+using namespace boost::filesystem;
 
-void compileFile(fs::path IDFolderWD, string ID, string fileName) {
-	//string thisFile = IDFolderWD.string();
-
-	//cout << fileName << " need compile" << endl;
-	//cout << IDFolderWD.string()<< endl;
-
-	string exeName = fileName.substr(0,fileName.find(".")) + ".exe";
-	fs::path exePath = IDFolderWD / exeName; string exePathStr = exePath.string();
-	fs::path filePath = IDFolderWD / fileName; string filePathStr = filePath.string();
-	string compileCmd = "g++ -o " + exePathStr + " " + filePathStr;
-	string runCmd = exePathStr;
-
-	 
-	cout << compileCmd << endl;
-	cout << runCmd << endl;
-
-	
+void compileFile(path FolderWD, string ID, string fileName,string sub) {
+	string objName = fileName.substr(0,fileName.find(".")) + ".obj";
+	//tao thu muc build
+	path temp{ FolderWD /ID/sub/ "build" };
+	create_directory(temp);
+	//noi chua file .obj
+	path objPath = temp / objName;
+	//noi chua file .cpp
+	path cppPath = FolderWD /ID/sub/ fileName;
+	string compileCmd = "g++ -o " + objPath.string() + " " + cppPath.string();
 	system(compileCmd.c_str());
-	system(runCmd.c_str());
 }
-
-bool checkFolderNotExistOrChange(fs::path workingDir, fs::path submitFol,
-	string IDStudent) {
-	fs::path IDFolderInWorkingDir = workingDir / IDStudent;
-	fs::path IDFolderInSubmitFol = submitFol / IDStudent;
-	if (!fs::exists(IDFolderInWorkingDir)) {//this is a brand new IDFolder
-		fs::copy(IDFolderInSubmitFol, IDFolderInWorkingDir);	
-	}
-		
-	//iterate from first file in submitFolder to end, copy all of them to
-	//the corresponse folder in workingDir 
-	for (fs::directory_iterator file(IDFolderInSubmitFol);
-		file != fs::directory_iterator(); ++file) {
-		
+bool Copyfile(path workingDir, path submitFol,string ID,string sub) {
+	int quantity = 0;//so luong file
+	int count = 0;//dem
+	path IDFolderInWorkingDir = workingDir /ID/sub;
+	path IDFolderInSubmitFol = submitFol /ID/sub;
+		//this is a brand new IDFolder
+	create_directory(workingDir / ID);
+	create_directory(workingDir / ID/sub);
+	for (directory_iterator file(IDFolderInSubmitFol);file != directory_iterator(); ++file) {
+		//dem file da duoc doc sang
+		count++;
 		string curFileName = file->path().filename().string();
-		fs::path desFileName = workingDir /IDStudent/curFileName;
-		if (!fs::exists(desFileName) ||
-			fs::exists(desFileName) &&
-			fs::file_size(desFileName) != fs::file_size(file->path())) {
+		path desFileName = workingDir /ID/sub/curFileName;
+		if (!exists(desFileName) ||
+			exists(desFileName) &&
+			file_size(desFileName) != file_size(file->path())) {
+			copy(file->path(), workingDir / ID/sub/ curFileName);
+		}
 
-			fs::copy_file(file->path(), workingDir / IDStudent / curFileName,fs::copy_option::overwrite_if_exists);
-
-			compileFile(workingDir / IDStudent, IDStudent, curFileName);
+		//tim so luong file
+		if (!curFileName.compare("pro.xml")) {
+			//chuyen string ve char* de dung ham doc
+			char * writable = new char[desFileName.string().size() + 1];
+			for (int i = 0; i < desFileName.string().length()+1; i++) {
+				if (i == desFileName.string().length()) writable[i] = '\0';
+				else writable[i] = desFileName.string()[i];
+				
+			}
+			TiXmlDocument doc(writable);
+			if (!doc.LoadFile())
+			{
+				//printf("%s", doc.ErrorDesc());
+				return 0;
+			}
+			TiXmlElement* root = doc.RootElement();
+			TiXmlElement* child1 = root->FirstChildElement();
+			quantity=atoi(child1->GetText());
+			if (quantity <= count) return true;
+		}
+	}
+	return false;
+}
+bool CompilethroughXML(const char* link,path workingDir,string ID,string sub) {
+	TiXmlDocument doc(link);
+	if (!doc.LoadFile())
+	{
+		//printf("%s", doc.ErrorDesc());
+		return 0;
+	}
+	TiXmlElement* root = doc.RootElement();
+	TiXmlElement* child1 = root->FirstChildElement();
+	//cout << child1->ValueTStr().c_str();
+	while (child1) {
+		string path = child1->GetText();
+		if (path.find(".") < path.length()) {
+			string cpp = path.substr(path.find(".") + 1, cpp.length() - 1);
+			if (!cpp.compare("cpp"))
+			{
+				compileFile(workingDir, ID, path,sub);
+			}
+		}
+		child1 = child1->NextSiblingElement();
+	}
+	return 1;
+}
+bool checkID(avlTree &dataID, path submitFolder,path workingDir) {
+	//duyet tuan tu file submitFolder
+	for (directory_iterator file(submitFolder); file != directory_iterator(); ++file) {
+		string IDNameFolder = file->path().filename().string();//lay ra ten ID
+		//ID bo vao database neu chua co
+		if (!dataID.search(IDNameFolder)) dataID.AVLInsert(dataID.root, new node(IDNameFolder, 0), dataID.taller);	
+		int count = 0;
+		//duyet tuan tu file sub
+		for (directory_iterator filesub(submitFolder/IDNameFolder); filesub != directory_iterator(); ++filesub) {
+				count++;
+				if (count > dataID.search(IDNameFolder)->numberSub) {
+					dataID.search(IDNameFolder)->numberSub++;
+					string ID = IDNameFolder;
+					string sub = "sub" + to_string(dataID.search(IDNameFolder)->numberSub);
+					//copy den khi nao du file thi thoi nho` vao quantity trong xml
+					while (1) {
+						if (Copyfile(workingDir, submitFolder, ID, sub) == true) break;
+					}
+					//-----compile nho file xml-----
+					path xml = workingDir / ID / sub / "pro.xml";
+					string xmlStr = xml.string();
+					char * writable = new char[xmlStr.length() + 1];
+					for (int i = 0; i < xmlStr.length() + 1; i++) {
+						if (i == xmlStr.length()) writable[i] = '\0';
+						else writable[i] = xmlStr[i];
+					}
+					CompilethroughXML(writable, workingDir, ID, sub);
+					//--------------------------------
+			}
 		}
 		
 	}
-	
-	return true;
+	return 1;
 }
-
 int main() {
-	fs::path workingDir("D:\\workingDir");
-	fs::path submitFolder("D:\\submitFolder");
-	string ID = "1610081";
-	fs::path IDFolder = submitFolder / ID;
-
-	checkFolderNotExistOrChange(workingDir, submitFolder, ID);
+	//path lam vc
+	path workingDir("D:\\workingDir");
+	path submitFolder("D:\\submitFolder");
+	//data base
+	avlTree DataID;
+	//check file
+	//thu tu uu tien la flie ID ben trong co file sub dau tien
+	while (1) {
+		checkID(DataID, submitFolder, workingDir);
+	}
 	system("pause");
 }
